dd9dd50e2f40df428bc0f17a391a2599e3a68d9f
Hammad740 <mohammadhammadansari91@gmail.com>
2023-12-02 00:56:55 +0530

    filtering

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -1,12 +1,37 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({ featured: true });
+  const products = await Product.find({}).sort('-name price');
 
-  res.status(200).json({ products });
+  res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: `products route` });
+  const { featured, company, name, sort, select } = req.query;
+  const queryObject = {};
+
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' };
+  }
+  let result = Product.find(queryObject);
+  ///// sort
+  if (sort) {
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('createdAt');
+  }
+  if (select) {
+    const selectList = select.split(',').join(' ');
+    result = result.select(selectList);
+  }
+  const products = await result;
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
