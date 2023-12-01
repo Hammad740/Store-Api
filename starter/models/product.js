2a26ebf74efe5d392256be14400ed7a70871486b
Hammad740 <mohammadhammadansari91@gmail.com>
2023-12-01 23:47:51 +0530

    routing

---
+++ starter/models/product.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Product name must be provided'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'Product price must be provided'],
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  company: {
+    type: String,
+
+    values: ['ikea', 'liddy', 'caressa', 'marcos'],
+    message: `{VALUE} is not supported`,
+    //enum: ['ikea', 'liddy', 'caressa', 'marcos'],
+  },
+});
+
+module.exports = mongoose.model('Product', productSchema);
